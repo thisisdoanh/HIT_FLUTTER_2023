1c9b5eb016caf57f2cf61124148603279943efb8
thisisdoanh <tranquocdoanhvq@gmail.com>
2023-03-24 22:33:33 +0700

    'BTVN3'

---
+++ Buoi3/freaking_math/build/app/generated/source/buildConfig/debug/com/example/freaking_math/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.freaking_math;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.freaking_math";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0";
+}
